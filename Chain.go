5b7ec7b5c8d420064e2e4c16856130e5203db1f5
Jagepard <dankorot@gmail.com>
2020-02-12 13:02:50 +0300

    updated

--- Chain.go
+++ Chain.go
@@ -1,10 +1,30 @@
 package main
 
+import "fmt"
+import "errors"
+
 // Chain is ...
 type Chain struct {
 	chain map[int]Handler
 }
 
 func (chain Chain) addToChain(handler Handler) {
+	if val, ok := chain.chain[handler.priority]; ok {
+		fmt.Println(errors.New(val.name + ":already exists"))
+		panic("Wrong argument!")
+	}
+
 	chain.chain[handler.priority] = handler
+}
+
+func (chain Chain) execute(handlerPriority int) {
+	if handler, ok := chain.chain[handlerPriority]; ok {
+		for i := 1; i <= handler.priority; i++ {
+			fmt.Println(chain.chain[i].name)
+		}
+
+		return
+	}
+
+	// fmt.Println(errors.New(handlerPriority + ": does not exist in the chain"))
 }
